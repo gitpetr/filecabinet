42a906f9317608cbcc92defad16c42da86f9a8cc
gitpetr <pkhilenko@yandex.ru>
2016-11-23 11:51:42 +0600

    add style to welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :docs
-  root 'welcome#index'
   authenticated :user do 
-    root "docs#index" as: 'authenticated_root'
+    root "docs#index", as: 'authenticated_root'
   end
+  root 'welcome#index'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
